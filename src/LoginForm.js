d39adb2e70a0e88e8ee5cd2dd94eb5e3ae7f780b
Excellence Chukwunaedu <99671287+Exconglo@users.noreply.github.com>
2022-06-27 16:43:06 +0100

    Assignment

---
+++ src/LoginForm.js
@@ -0,0 +1,20 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Login() {
+	const LoginForm =>{
+		
+	}
+	return (
+		<div>
+			<form>
+				<input placeholder={"Enter your account name"}></input>
+				<input type={password placeholder={"Enter your password"}></input>
+				<a href="signup.html">Sign up?</a>
+				<button type='button' onClick={"Save Password?"}>Login</button>
+			</form>
+		</div>
+	);
+}
+
+export default Login;*/
